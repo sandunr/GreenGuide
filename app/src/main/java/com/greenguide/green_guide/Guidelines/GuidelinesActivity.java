1fc4802f1440268758b5641e39dc3f57b9b931c7
Calvin <cwfei@uw.edu>
2017-11-16 22:47:09 -0800

    Calvin: Completed integration of Guidelines page. Please that I have not implemented a back button as of this moment

--- app/src/main/java/com/greenguide/green_guide/Guidelines/GuidelinesActivity.java
+++ app/src/main/java/com/greenguide/green_guide/Guidelines/GuidelinesActivity.java
@@ -2,6 +2,7 @@ package com.greenguide.green_guide.Guidelines;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.greenguide.green_guide.R;
 
@@ -11,5 +12,19 @@ public class GuidelinesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guidelines);
+
+        CharSequence[] g_Arr = getResources().getTextArray(R.array.guidelines_array);
+        ((TextView) findViewById(R.id.guidelines_title)).setText(g_Arr[0]);
+        ((TextView) findViewById(R.id.guide_intro_title)).setText(g_Arr[1]);
+        ((TextView) findViewById(R.id.guide_intro)).setText(g_Arr[2]);
+        ((TextView) findViewById(R.id.guide_body)).setText(g_Arr[3]);
+        ((TextView) findViewById(R.id.guide_trust_title)).setText(g_Arr[4]);
+        ((TextView) findViewById(R.id.guide_trust_body)).setText(g_Arr[5]);
+        ((TextView) findViewById(R.id.guide_security_title)).setText(g_Arr[6]);
+        ((TextView) findViewById(R.id.guide_rest)).setText(g_Arr[7]);
+        /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                g_Arr);*/
     }
 }
